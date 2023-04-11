a7b0afeb7d1cc007fba0dff74dbb1f31a69315ee
Pierre Abbat <phma@bezitopo.org>
2023-04-11 19:26:26 +0000

    Start defining the S-box

--- src/Cryptography/Daphne.hs
+++ src/Cryptography/Daphne.hs
@@ -1,5 +1,6 @@
 module Cryptography.Daphne
   ( twist
+  , funSbox
   ) where
 
 import Data.Bits
@@ -8,7 +9,8 @@ import Data.Word
 import qualified Data.Sequence as Seq
 import Data.Sequence ((><), (<|), (|>), Seq((:<|)), Seq((:|>)))
 
-twist :: Bits a => Num a => a -> a
-twist n = rotate n (popCount n)
+twist :: Bits a => Int -> a -> a
+twist k n = rotate n ((popCount n)*k)
 
--- funSbox :: Word8 -> Word8
+funSbox :: Word8 -> Word8
+funSbox = (xor 0x6e) . (twist (-1)) . (xor 0x25)
