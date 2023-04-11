f07022772efafc1252d5343256100309a1476ad0
Pierre Abbat <phma@bezitopo.org>
2023-04-11 08:53:34 +0000

    Start writing Daphne module

---
+++ src/Cryptography/Daphne.hs
@@ -0,0 +1,14 @@
+module Cryptography.Daphne
+  ( twist
+  ) where
+
+import Data.Bits
+import Data.Array.Unboxed
+import Data.Word
+import qualified Data.Sequence as Seq
+import Data.Sequence ((><), (<|), (|>), Seq((:<|)), Seq((:|>)))
+
+twist :: Bits a, Num a => a -> a
+twist n = rotate n (popCount n)
+
+-- funSbox :: Word8 -> Word8
