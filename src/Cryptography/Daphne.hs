c94306e15a6c4ae513507b5c07a15912b8a5519e
Pierre Abbat <phma@bezitopo.org>
2023-04-11 20:19:42 +0000

    Define S-box and its inverse as arrays

--- src/Cryptography/Daphne.hs
+++ src/Cryptography/Daphne.hs
@@ -1,7 +1,7 @@
 module Cryptography.Daphne
   ( twist
-  , shuffle
-  , funSbox
+  , sbox
+  , invSbox
   ) where
 
 import Data.Bits
@@ -19,3 +19,11 @@ shuffle n = (rotate (n .&. 0x54) 3) .|. (rotate (n .&. 0x28) 7) .|.
 
 funSbox :: Word8 -> Word8
 funSbox = (xor 0x6e) . shuffle . (twist (-1)) . (xor 0x25)
+
+sbox = array (0,255)
+  [ (i,funSbox i) | i <- [0..255] ]
+  :: Array Word8 Word8
+
+invSbox = array (0,255)
+  [ (funSbox i,i) | i <- [0..255] ]
+  :: Array Word8 Word8
