d7ea3df68045e8877f953a2c093dcc81c49ce240
Pierre Abbat <phma@bezitopo.org>
2023-04-11 20:07:20 +0000

    Define the bit permutation of the S-box

--- src/Cryptography/Daphne.hs
+++ src/Cryptography/Daphne.hs
@@ -1,5 +1,6 @@
 module Cryptography.Daphne
   ( twist
+  , shuffle
   , funSbox
   ) where
 
@@ -12,5 +13,9 @@ import Data.Sequence ((><), (<|), (|>), Seq((:<|)), Seq((:|>)))
 twist :: Bits a => Int -> a -> a
 twist k n = rotate n ((popCount n)*k)
 
+shuffle :: Word8 -> Word8
+shuffle n = (rotate (n .&. 0x54) 3) .|. (rotate (n .&. 0x28) 7) .|.
+            (rotate (n .&. 0x02) 5) .|. (rotate (n .&. 0x80) 4) .|. (n .&. 1)
+
 funSbox :: Word8 -> Word8
-funSbox = (xor 0x6e) . (twist (-1)) . (xor 0x25)
+funSbox = (xor 0x6e) . shuffle . (twist (-1)) . (xor 0x25)
