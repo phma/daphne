931d5a8e6a7020f6d7eb8cb17827ed46efa47ffc
Pierre Abbat <phma@bezitopo.org>
2023-05-07 08:16:03 +0000

    Add twist function in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn twist(byte: u8) -> u8 {
+  byte.rotate_left(byte.count_ones())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
