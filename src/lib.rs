7574cf062acd81e7304911e54a32daefffb894cd
Pierre Abbat <phma@bezitopo.org>
2023-05-07 19:28:32 +0000

    Compute the S-box as a Rust function

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,24 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-pub fn twist(byte: u8) -> u8 {
-  byte.rotate_left(byte.count_ones())
+fn twist(n: u8,k: isize) -> u8 {
+  if k>0 {
+    n.rotate_left(n.count_ones())
+  } else {
+    n.rotate_right(n.count_ones())
+  }
+}
+
+fn shuffle(n: u8) -> u8 {
+  (n&0x54).rotate_left(3) |
+  (n&0x28).rotate_left(7) |
+  (n&0x02).rotate_left(5) |
+  (n&0x80).rotate_left(4) |
+  (n&0x01)
+}
+
+pub fn funSbox(n: u8) -> u8 {
+  0x6e^shuffle(twist(n^0x25,-1))
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use daphne::twist;
+use daphne::funSbox;
 
 fn main() {
   for i in 0..=255 {
-    print!("{:02x} ",twist(i));
+    print!("{:02x} ",funSbox(i));
     if i%16==15 {
       println!();
     }
