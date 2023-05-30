2a4adbcd1ff6393dccf5dde6800d3c847915d14b
Pierre Abbat <phma@bezitopo.org>
2023-05-30 09:14:35 +0000

    Test in Haskell that step and invStep are inverses

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,24 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE InstanceSigs #-}
+import Data.Word
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit
+import Cryptography.Daphne
+
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [properties, unitTests]
+
+properties :: TestTree
+properties = testGroup "Properties" [qcProps]
+
+qcProps = testGroup "(checked by QuickCheck)"
+  [ QC.testProperty "invStep" $
+      \x l r -> invStep (step x l r) l r == x
+  ]
+
+unitTests = testGroup "Unit tests"
+  [ testCase "step49" $
+      (step 49 243 125) @?= 75
+  ]
