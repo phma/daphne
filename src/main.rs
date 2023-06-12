801001bd332ff6cf4be81fc542bcea6186668588
Pierre Abbat <phma@bezitopo.org>
2023-06-12 08:16:56 +0000

    Bring out the printing loop to a function printvec

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use daphne::*;
 
-fn main() {
-  let mut zerodaph=Daphne::new();
-  let mut squaredaph=Daphne::new();
-  squaredaph.set_key(&[0,1,4,9,16,25,36,49,64,81,100,121,144,169,196,225]);
-  for i in 0..=255 {
-    print!("{:02x} ",step(i,243,125));
-    if i%16==15 {
+fn printvec(k:&[u8]) {
+  for i in 0..k.len() {
+    print!("{:02x} ",k[i]);
+    if i%16==15 || i+1==k.len() {
       println!();
     }
   }
+}
+
+fn main() {
+  let mut zerodaph=Daphne::new();
+  let mut squaredaph=Daphne::new();
+  let mut stepvec:Vec<u8> = Vec::new();
+  zerodaph.set_key(&[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+  squaredaph.set_key(&[0,1,4,9,16,25,36,49,64,81,100,121,144,169,196,225]);
+  for i in 0..=255 {
+    stepvec.push(step(i,243,125));
+  }
+    printvec(&stepvec);
 }
