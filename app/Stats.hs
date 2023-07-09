f9c734a8a92429bf7185b4f5f71391a5ee2d1043
Pierre Abbat <phma@bezitopo.org>
2023-07-09 09:59:35 +0000

    Count one output bit of a SAC histogram

--- app/Stats.hs
+++ app/Stats.hs
@@ -2,9 +2,11 @@ module Stats
   ( Histo (..)
   , emptyHisto
   , hCount
+  , sacCountBit
   ) where
 
 import Data.Word
+import Data.Bits
 import qualified Data.Sequence as Seq
 import Data.Sequence ((><), (<|), (|>), Seq((:<|)), Seq((:|>)))
 import Data.Foldable (toList)
@@ -16,3 +18,11 @@ emptyHisto n = Histo (Seq.replicate n 0)
 
 hCount :: Int -> Histo -> Histo
 hCount n (Histo h) = Histo (Seq.adjust' (+1) n h)
+
+squareWave :: Int -> [Int]
+squareWave n = map (((-1) ^) . (.&. (1::Int)) . (`shift` (-n))) [0..]
+
+sacCountBit :: Histo -> Int -> Int
+-- The histogram is produced by a strict avalanche criterion count; i.e. each
+-- number counted is the xor of two outputs whose inputs differ by one bit.
+sacCountBit (Histo h) n = sum $ zipWith (*) (squareWave n) (map fromIntegral (toList h))
